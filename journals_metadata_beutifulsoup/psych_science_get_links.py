63ea555e3cc30c2525c2a216a28d69bd23d6ef01
Dominik Lenda <dlenda1729@gmail.com>
2020-01-30 21:16:40 +0100

    Remove redundant files.

--- journals_metadata_beutifulsoup/psych_science_get_links.py
+++
@@ -1,7 +0,0 @@
-https://journals.sagepub.com/loi/PSS?year=2015
-
-https://journals.sagepub.com/toc/pssa/26/1
-
-https://journals.sagepub.com/templates/jsp/_style2/_sage/images/open-data.png
-
-https://journals.sagepub.com/templates/jsp/_style2/_sage/images/open-material.png
